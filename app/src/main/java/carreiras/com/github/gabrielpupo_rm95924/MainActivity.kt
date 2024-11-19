41304c53827a72775946c14ce23a2e6e6d370f37
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-18 21:44:20 -0300

    Sqlite Iniciado

--- app/src/main/java/carreiras/com/github/gabrielpupo_rm95924/MainActivity.kt
+++ app/src/main/java/carreiras/com/github/gabrielpupo_rm95924/MainActivity.kt
@@ -11,20 +11,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var dicaAdapter: DicaAdapter
     private lateinit var searchView: SearchView
-    private val listaDicas = mutableListOf(
-        Dica("Desligue aparelhos que não estão em uso", "Aparelhos eletrônicos consomem energia mesmo em modo de espera. Desconecte quando não for usar."),
-        Dica("Use lâmpadas LED", "Lâmpadas LED consomem até 80% menos energia que as lâmpadas incandescentes."),
-        Dica("Aproveite a luz natural", "Abra as cortinas e janelas para maximizar a luz natural e reduzir o uso de energia elétrica."),
-    )
+    private lateinit var dicaDAO: DicaDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        dicaDAO = DicaDAO(this)
+
         recyclerView = findViewById(R.id.recyclerView)
         searchView = findViewById(R.id.searchView)
 
+        val listaDicas = dicaDAO.getAllDicas()
         dicaAdapter = DicaAdapter(listaDicas)
+
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = dicaAdapter
 
@@ -34,7 +34,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                val filteredList = listaDicas.filter {
+                val filteredList = dicaDAO.getAllDicas().filter {
                     it.titulo.contains(newText ?: "", ignoreCase = true) ||
                             it.descricao.contains(newText ?: "", ignoreCase = true)
                 }
